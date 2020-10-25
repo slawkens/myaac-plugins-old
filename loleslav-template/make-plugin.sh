2c00bff4dff1531acd324bf88a79f80468e45b36
slawkens <slawkens@gmail.com>
2020-10-25 23:39:18 +0100

    loleslav-template: Ignore file that is almost 1.3MB big
    PHP has default limit of 2MB

--- loleslav-template/make-plugin.sh
+++ loleslav-template/make-plugin.sh
@@ -1,2 +1,2 @@
 rm -f myaac-loleslav-template.zip
-zip -r myaac-loleslav-template.zip templates/ plugins/ -x */\.*
+zip -r myaac-loleslav-template.zip templates/ plugins/ -x */\.* -x "templates/loleslav/sources/bg03.psd"
