fc02f3685f131d2a144e7b1aa634ac9c06ee8b9f
xitobuh <jonas.hockert92@gmail.com>
2021-05-30 13:52:43 +0000

    initial submission

---
+++ reborn-template/templates/reborn/config.php
@@ -0,0 +1,8 @@
+<?php
+$config['table_header_bg'] = '#212529'; //Background color for headers
+$config['table_header_textcolor'] = "white"; //Color for header text
+$config['header-logo'] = "reborn-logo.png"; //Place picture in reborn/images
+$config['darkborder'] = '#fff'; //required for myaac
+$config['lightborder'] = '#fff'; //required for myaac
+$config['vdarkborder'] = '#212529'; //set same as table_header_bg - required for myaac
+?>
