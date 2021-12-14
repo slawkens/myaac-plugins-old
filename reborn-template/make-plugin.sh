54a41c19620b98248d349e32baf5a7587c29031c
slawkens <slawkens@gmail.com>
2021-12-14 20:15:14 +0100

    reborn-templaet: updates

---
+++ reborn-template/make-plugin.sh
@@ -0,0 +1,2 @@
+rm -f myaac-reborn-template.zip
+zip -r myaac-reborn-template.zip templates/ plugins/ -x */\.*
---
+++ reborn-template/plugins/reborn-template/menus.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+	0 => [ // Top Menu
+		'News' => 'news',
+		'Create Account' => 'account/create',
+		'Downloads' => 'downloads',
+		'Forum' => 'forum',
+	],
+	1 => [ // Home
+		'Latest News' => 'news',
+		'News Archive' => 'news/archive',
+		'Downloads' => 'downloads',
+		'Rules' => 'rules',
+		'Support' => 'team',
+		'FAQ' => 'faq',
+	],
+	2 => [ // Community
+		'Characters' => 'characters',
+		'Online' => 'online',
+		'Highscores' => 'highscores',
+		'Last Kills' => 'lastkills',
+		'Houses' => 'houses',
+		'Guilds' => 'guilds',
+		'Bans' => 'bans',
+		'Forum' => 'forum',
+	],
+	3 => [ // Library
+		'Creatures' => 'creatures',
+		'Spells' => 'spells',
+		'Commands' => 'commands',
+		'Server Info' => 'serverInfo',
+		'Gallery' => 'gallery',
+		'Experience Table' => 'experienceTable',
+	],
+	4 => [ // Highscores
+		'Level' => 'highscores/experience',
+		'Magic' => 'highscores/magic',
+		'Shielding' => 'highscores/shielding',
+		'Distance' => 'highscores/distance',
+		'Club' => 'highscores/club',
+		'Sword' => 'highscores/sword',
+		'Axe' => 'highscores/axe',
+		'Fist' => 'highscores/fist',
+		'Fishing' => 'highscores/fishing',
+	],
+	5 => [ // Shop
+		'Buy Points' => 'points',
+		'Gifts' => 'gifts',
+		'History' => 'gifts/history',
+	],
+];
