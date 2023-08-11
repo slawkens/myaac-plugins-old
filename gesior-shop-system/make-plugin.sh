04590ad2d11166987b6f0d79289f652ea0f00809
slawkens <slawkens@gmail.com>
2023-08-11 22:39:22 +0200

    Add version prefix

--- gesior-shop-system/make-plugin.sh
+++ gesior-shop-system/make-plugin.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 VERSION=`cat plugins/gesior-shop-system.json | jq -r '.version'`
-rm -f myaac-gesior-shop-system-$VERSION.zip
-zip -r myaac-gesior-shop-system-$VERSION.zip lua/ admin/ system/ payments/ plugins/ -x */\.*
+rm -f myaac-gesior-shop-system-v$VERSION.zip
+zip -r myaac-gesior-shop-system-v$VERSION.zip lua/ admin/ system/ payments/ plugins/ -x */\.*
--- rasta-template/make-plugin.sh
+++ rasta-template/make-plugin.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 VERSION=`cat plugins/rasta-template.json | jq -r '.version'`
-rm -f myaac-rasta-template-$VERSION.zip
-zip -r myaac-rasta-template-$VERSION.zip templates/ plugins/ -x */\.*
+rm -f myaac-rasta-template-v$VERSION.zip
+zip -r myaac-rasta-template-v$VERSION.zip templates/ plugins/ -x */\.*
