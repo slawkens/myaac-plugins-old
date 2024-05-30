b67924d211b449c1da7297ad8e9e83651acaa17b
slawkens <slawkens@gmail.com>
2024-05-30 14:15:39 +0200

    delete google-recaptcha, in 0.8 it's built in

--- google-recaptcha/make-plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-VERSION=`cat plugins/google-recaptcha.json | jq -r '.version'`
-NAME=myaac-google-recaptcha-v$VERSION.zip
-rm -f $NAME
-zip -r $NAME plugins/ -x */\.*
--- google-recaptcha/plugins/google-recaptcha/hooks/display-recaptcha.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MYAAC') or die('Direct access not allowed!');
-
-require_once __DIR__ . '/../init.php';
-
-if(GoogleReCAPTCHA::enabled()) {
-	$twig->display('google-recaptcha/templates/recaptcha-display.html.twig');
-}
