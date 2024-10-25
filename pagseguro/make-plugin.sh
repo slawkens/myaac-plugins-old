e82441557a0c1c4b0d363593abebace57b9ae2cc
slawkens <slawkens@gmail.com>
2024-10-25 12:17:07 +0200

    Remove pagseguro plugin, its outdated

--- pagseguro/make-plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-VERSION=`cat plugins/pagseguro.json | jq -r '.version'`
-NAME=myaac-pagseguro-v$VERSION.zip
-rm -f $NAME
-zip -r $NAME images/ system/ payments/ plugins/ -x */\.*
